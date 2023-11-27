d03accb2d19b2cd819e92ce1a5d53b0db0d9ac23
Illia Korniychik <korniykhik3@gmail.com>
2023-11-27 14:44:31 +0200

    Done

---
+++ BookManager.Tests/BookTests.cs
@@ -0,0 +1,58 @@
+﻿using BookManagerProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManager.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Equals_InstanceWithTheSameFieldsAreEquals_ReturnTrue()
+        {
+            //Arrange 
+            var sut = new Book("Harry Potter", "Joahn Rowling", "Fantasy");
+            
+            var expected = true;
+            
+            //Act
+
+            var actual = sut.Equals(new Book("Harry Potter", "Joahn Rowling", "Fantasy"));
+
+            //Arrange
+            Assert.Equal(expected, actual);
+        
+        }
+
+        [Fact]
+        public void Equals_InstancesWithDifferentFieldsAreNotEquals_ReturnFalse()
+        {
+            //Arrange
+            var sut = new Book("SomeTitle", "SomeAuthor", "Genre");
+
+            var expected = false;
+
+            //Act
+            var actual = sut.Equals(new Book("Title", "Author", "Genre"));
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToString_CorrectToStringFormat()
+        {
+            //Arrange
+            var sut = new Book("Harry Potter", "Joahn Rowling", "Fantasy");
+            var expected = "Harry Potter by Joahn Rowling. Fantasy";
+            
+            //Act
+            var actual = sut.ToString();
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ BookManagerProject/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManagerProject
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+
+        public Book() { }
+
+        public Book(string title, string author, string genre)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} by {Author}. {Genre}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if ( obj == null) return false;
+            if (obj is not Book ) { return false; }
+            
+            return ( (Title.Equals(((Book)obj).Title)) && (Author.Equals(((Book)obj).Author)) && (Genre.Equals(((Book)obj).Genre)));
+        }
+    }
+}
